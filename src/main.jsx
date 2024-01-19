cb3214ce807bc60184cc86b3820c3da331547493
Andrew <andrewlin1368@gmail.com>
2024-01-18 20:13:59 -0500

    added incorrect url redirect

--- src/main.jsx
+++ src/main.jsx
@@ -22,6 +22,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
             path="/players/new"
             element={<PlayerForm></PlayerForm>}
           ></Route>
+          <Route path="*" element={<App></App>}></Route>
         </Routes>
       </Provider>
     </BrowserRouter>
